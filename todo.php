9c315f105d63d481d5b1da49191390faacc388a8
Armando Salazar <armandosalazar351@gmail.com>
2022-09-10 22:22:38 -0500

    Add styles to TODO app

--- todo.php
+++ todo.php
@@ -3,20 +3,95 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>TODO</title>
+  <style>
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      background-color: #D4F1EF;
+      color: #166E6A;
+    }
+    .todoapp {
+      width: 500px;
+      margin: 30px auto 0;
+      padding: 30px;
+      text-align: center;
+    }
+
+    .todoapp>form {
+      margin-bottom: 20px;
+    }
+
+    .todoapp h1 {
+      margin-bottom: 20px;
+      text-align: left;
+    }
+
+    input[type="text"] {
+      outline: none;
+      padding: 10px 25px;
+      border-radius: 4px;
+      border: 1px solid #cccccc;
+      font-size: inherit;
+    }
+
+    input[type="submit"] {
+      font-family: inherit;
+      background-color: transparent;
+      padding: 5px 10px;
+      border-radius: 4px;
+      border: none;
+    }
+
+    .todo {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: #46484F;
+      background-color: #fff;
+      margin-bottom: 1px;
+      padding: 10px;
+    }
+
+    /*
+    .todolist .todo:nth-child(even) {
+      background-color: #ccc;
+    }
+    */
+
+    input.delete {
+      background-color: #ce0c00;
+      color: #fff;
+      font-weight: bold;
+      padding: 10px 15px;
+    }
+    input.add {
+      background-color: #166E6A;
+      color: #fff;
+      font-weight: bold;
+      padding: 10px 15px;
+      font-size: inherit;
+    }
+  </style>
 </head>
 
 <body>
+<div class="todoapp">
+  <h1>TODO App</h1>
   <form action="">
     <label>
       <b>
-        TODO
+        TODO:
       </b>
       <input type="text" name="text">
     </label>
-    <input type="submit" value="Add">
+    <input type="submit" value="Add" class="add">
   </form>
   <div class="todolist">
     <?php
@@ -44,29 +119,38 @@
         $sql = "UPDATE todo SET status = true WHERE id = $id";
       }
       $connection->query($sql);
+    } elseif (isset($_GET['delete'])) {
+      $id = $_GET['delete'];
+      $connection->query("DELETE FROM todo WHERE  id = $id");
     }
-    $sql = "SELECT * FROM todo WHERE status = false";
+    $sql = "SELECT * FROM todo WHERE status = false ORDER BY text ASC";
     $result = $connection->query($sql);
     if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
         // var_dump($row);
         // print_r($row);
-    ?>
-        <div>
+        ?>
+        <div class="todo">
           <form action="" id="todo-<?php echo $row['id'] ?>">
             <label>
-              <input id="<?php echo $row['id'] ?>" type="checkbox" onchange="completed(this)" name="id" value="<?php echo $row['id']; ?>">
+              <input id="<?php echo $row['id'] ?>" type="checkbox" onchange="completed(this)" name="id"
+                     value="<?php echo $row['id']; ?>">
               <?php echo $row['text'] ?>
             </label>
           </form>
+          <form action="" id="todo-delete-<?php echo $row['id']; ?>">
+            <input type="hidden" name="delete" value="<?php echo $row['id'] ?>">
+            <input type="submit" value="Delete" class="delete">
+          </form>
         </div>
-    <?php
+        <?php
       }
     }
 
     $connection->close();
     ?>
   </div>
+</div>
 </body>
 <script>
   function completed(evt) {
